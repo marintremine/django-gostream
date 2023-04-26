668c40c4f5b69f52be8d953ae43164c58cac65f4
marin.tremine <marin.tremine@etu.univ-orleans.fr>
2023-04-26 13:01:39 +0200

    partenaire réseaux

--- gostream/partenaire/models.py
+++ gostream/partenaire/models.py
@@ -5,6 +5,14 @@ class Partenaire(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True)
     pp = models.ImageField(upload_to='partenaires', blank = True, null= True)
+    
+    twitch = models.CharField(max_length=256,blank = True)
+    youtube = models.CharField(max_length=256,blank = True)
+    twitter = models.CharField(max_length=256,blank = True)
+    instagram = models.CharField(max_length=256,blank = True)
+    tiktok = models.CharField(max_length=256,blank = True)
+    discord = models.CharField(max_length=256,blank = True)
+    other = models.CharField(max_length=256,blank = True)
 
     def __str__(self) -> str:
         return self.name
