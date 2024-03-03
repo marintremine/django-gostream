dfb7e5e90ea88b1662816523d4ea71871bb58935
marintremine <extrayer.dev@gmail.com>
2024-03-03 12:28:27 +0100

    update 03/03/2024

--- gostream/equipe/admin.py
+++ gostream/equipe/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from equipe.models import Equipe, Streamer
+from equipe.models import Equipe, Streamer, Intervenant
 
 # Register your models here.
 
 
 admin.site.register(Streamer)
 admin.site.register(Equipe)
+admin.site.register(Intervenant)
--- gostream/equipe/views.py
+++ gostream/equipe/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from equipe.models import Equipe, Streamer
+from equipe.models import Equipe, Streamer, Intervenant
 from django.db.models.functions import Lower
 # Create your views here.
 
@@ -8,4 +8,5 @@ from django.db.models.functions import Lower
 def equipe(request):
     streamers = Streamer.objects.order_by(Lower("name"))
     equipes = Equipe.objects.order_by("ordre")
-    return render(request,'equipe/index.html', context={'streamers':streamers,'equipes':equipes})
+    intervenants = Intervenant.objects.order_by(Lower("name"))
+    return render(request,'equipe/index.html', context={'streamers':streamers,'equipes':equipes,'intervenants':intervenants})
